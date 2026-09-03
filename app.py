5edbb48463dbc2fd827df82b1bb4764cea501610
Guilherme Fraga <119956812+GSouzaDev20@users.noreply.github.com>
2026-09-03 17:04:20 +0000

    feat: Add form validation and required fields for expense description and value in the index.html template.

--- app.py
+++ app.py
@@ -4,18 +4,23 @@ from database import buscar_gastos, salvar_gasto
 
 app = Flask(__name__)
 
+
 def Saldo_diario(Salario, Despesas, Valor_para_guardar):
     Saldo = Salario - Despesas - Valor_para_guardar
     return Saldo
+
 
 def Limite_diario(Valor_restante, DiaRestantes):
     if DiaRestantes <= 0:
         return 0
     Limite = Valor_restante / DiaRestantes
     return Limite
+
+
 def Despesas(Gastos_Mensais):
     total_despesas = sum(gasto["valor"] for gasto in Gastos_Mensais)
     return total_despesas
+
 
 @app.route('/')
 def start():
@@ -29,26 +34,35 @@ def start():
 
     DiaRestantes = 25
     limite_diario = Limite_diario(Valor_restante, DiaRestantes)
-    return render_template('index.html', 
-                           Salario=Salario, 
-                           Despesas=despesas,
-                           Despesas_Detalhadas=gastos_detalhados, 
-                           Valor_para_guardar=Valor_para_guardar, 
-                           Valor_restante=Valor_restante,
-                           DiaRestantes=DiaRestantes,
-                           Limite_diario=limite_diario)
+    return render_template(
+        'index.html',
+        Salario=Salario,
+        Despesas=despesas,
+        Despesas_Detalhadas=gastos_detalhados,
+        Valor_para_guardar=Valor_para_guardar,
+        Valor_restante=Valor_restante,
+        DiaRestantes=DiaRestantes,
+        Limite_diario=limite_diario,
+    )
+
+
 @app.route('/adicionar_gasto', methods=['POST'])
 def adicionar_gasto():
     descricao = request.form['descricao']
     if not descricao:
         return "Descrição do gasto não pode ser vazia.", 400
+
     try:
         valor = float(request.form['valor'])
     except ValueError:
         return "Valor do gasto deve ser um número válido.", 400
-    # Aqui você pode adicionar a lógica para salvar o gasto no banco de dados
+
+    if valor <= 0:
+        return "Valor do gasto deve ser maior que zero.", 400
+
     salvar_gasto(descricao, valor)
     return redirect('/')
 
+    
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
